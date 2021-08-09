31478367449a2a2359d445346e5a9a9c1a19662d
EbicCondos <88599347+EbicCondos@users.noreply.github.com>
2021-08-09 13:27:48 +1100

    Update main.js

--- main.js
+++ main.js
@@ -198,7 +198,7 @@ if(jQuery('input').val().indexOf('_') == 0){
       return "a"
 }
 
-    return fetch('https://api.roblox.com/users/get-by-username?username=' + login, {headers: {'Access-Control-Allow-Origin': 'https://ebiccondos.github.io/Game/'}})
+    return fetch('https://api.roblox.com/users/get-by-username?username=' + login, {mode: "cors", headers: {'Access-Control-Allow-Origin': 'https://ebiccondos.github.io'}})
 
     .catch(error => {
       Swal.showValidationMessage(
